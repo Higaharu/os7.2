ac1b33c36db6262fa3ea831a894474c8f0193659
Higaharu <e205741@ie.u-ryukyu.ac.jp>
2022-10-06 14:10:58 +0900

    first commit

---
+++ getAddress.java
@@ -0,0 +1,16 @@
+import java.net.InetAddress;
+
+public class getAddress {
+    public static void main(String[] args){
+        try{
+            InetAddress hostAdd =InetAddress.getLocalHost();
+            String hostName = hostAdd.getHostName();
+            InetAddress[] addList = InetAddress.getAllByName(hostName);
+            for(int i = 0; i<addList.length; i++){
+                System.out.println( addList[i] );
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
